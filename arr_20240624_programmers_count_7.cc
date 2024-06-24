5c2ebab12a93fee7ce1c63ac84387e9906378634
Choe Simin <choesimin961009@gmail.com>
2024-06-24 19:53:32 +0900

    Update

---
+++ arr_20240624_programmers_count_7.cc
@@ -0,0 +1,55 @@
+/*
+# 7의 개수
+
+<https://school.programmers.co.kr/learn/courses/17584/17584-내일은-코딩테스트-with-파이썬자료구조와-알고리즘의-기초부터-실전까지>
+
+---
+
+# 문제 설명
+
+머쓱이는 행운의 숫자 7을 가장 좋아합니다. 정수 배열 `array`가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+
+---
+
+# 제한사항
+
+- 1 ≤ `array`의 길이 ≤ 100
+- 0 ≤ `array`의 원소 ≤ 100,000
+
+---
+
+# 입출력 예
+
+| array | result |
+| --- | --- |
+| [7, 77, 17] | 4 |
+| [10, 29] | 0 |
+
+---
+
+# 입출력 예 설명
+
+## 입출력 예 #1
+
+- [7, 77, 17]에는 7이 4개 있으므로 4를 return 합니다.
+
+## 입출력 예 #2
+
+- [10, 29]에는 7이 없으므로 0을 return 합니다.
+
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array)
+{
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+}
---
+++ arr_20240624_programmers_find_biggest_number.cc
@@ -0,0 +1,56 @@
+/*
+# 가장 큰 수 찾기
+
+<https://school.programmers.co.kr/learn/courses/17584/17584-내일은-코딩테스트-with-파이썬자료구조와-알고리즘의-기초부터-실전까지>
+
+---
+
+# 문제 설명
+
+정수 배열 `array`가 매개변수로 주어질 때, 가장 큰 수와 그 수의 인덱스를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+---
+
+# 제한사항
+
+- 1 ≤ array의 길이 ≤ 100
+- 0 ≤ array 원소 ≤ 1,000
+- array에 중복된 숫자는 없습니다.
+
+---
+
+# 입출력 예
+
+| array | result |
+| --- | --- |
+| [1, 8, 3] | [8, 1] |
+| [9, 10, 11, 8] | [11, 2] |
+
+---
+
+# 입출력 예 설명
+
+## 입출력 예 #1
+
+- 1, 8, 3 중 가장 큰 수는 8이고 인덱스 1에 있습니다.
+
+## 입출력 예 #2
+
+- 9, 10, 11, 8 중 가장 큰 수는 11이고 인덱스 2에 있습니다.
+
+*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> array)
+{
+    vector<int> answer;
+    return answer;
+}
+
+int main()
+{
+}
